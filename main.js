c432caf87e76262d3c291a1da427577d5044c5dd
Abdul Basit <abdulbasitbasit076@gmail.com>
2023-08-31 00:42:32 +0500

    fix issues

--- main.js
+++ main.js
@@ -5,11 +5,10 @@ const options = async () => {
     try {
         const res = await fetch('https://restcountries.com/v3/all');
         const data = await res.json();
-        console.log(data);
-        const currencyCodes = new Set(); 
+        const currencyCodes = new Set();
+        
         data.forEach(country => {
             const currencies = country.currencies;
-            // console.log(currencies);
             if (currencies) {
                 Object.keys(currencies).forEach(currencyCode => {
                     currencyCodes.add(currencyCode);
@@ -18,15 +17,20 @@ const options = async () => {
         });
 
         Array.from(currencyCodes).forEach(currencyCode => {
+
             const optionFrom = document.createElement('option');
             const optionTo = document.createElement('option');
+            
             optionFrom.value = currencyCode;
             optionFrom.textContent = currencyCode;
             optionTo.value = currencyCode;
             optionTo.textContent = currencyCode;
+            
             FromCountrySelect.appendChild(optionFrom);
             ToCountrySelect.appendChild(optionTo);
         });
+
+        //Set the flag
 
         FromCountrySelect.addEventListener("change", () => {
             const selectedCountry = data.find(country => {
@@ -51,35 +55,20 @@ const options = async () => {
             console.log(ToselectedCountry);
             document.getElementById('ToFlags').innerHTML = `<img src="${ToselectedCountry.flags[0]}">`;
         });
-        
 
     } catch (e) {
         console.log(e);
     }
 }
-
 options();
 
-let countriesData;
-
-const restCountries = async () => {
-    try{
-        const res = await fetch('https://restcountries.com/v3/all');
-        countriesData = await res.json();
-        // console.log(countriesData);
-    }catch(e){
-        console.log(e);
-    }
-}
-
-restCountries();
 
 const fetchCurrencyData = async (currency, base_currency) => {
     const Ammount = document.getElementById("Enter_Ammount");
     const Display = document.getElementById("display");
     const Flags = document.getElementById("Flags");
     const API_Key_URL = 'cur_live_0OqDn8Qiu3C8zcZGeqWjpo6yNMqHpur0mZaqTQVy';
-    if(base_currency === '' || currency === '' || base_currency === currency || Ammount.value === ''){
+    if (base_currency === '' || currency === '' || base_currency === currency || Ammount.value === '') {
         alert('Please Select two Different Currency');
         return
     }
@@ -98,15 +87,15 @@ const fetchCurrencyData = async (currency, base_currency) => {
     }
 }
 
+
+// main fubction to send data
 const fetchData = () => {
 
     let currency = ToCountrySelect.value;
     let base_currency = FromCountrySelect.value;
-    // let countryFlags = FromCountrySelect.value
 
     FromCountrySelect.addEventListener('change', (event) => {
         base_currency = event.target.value;
-        // console.log(event.target.value);
     });
 
     ToCountrySelect.addEventListener('change', (event) => {
@@ -115,5 +104,3 @@ const fetchData = () => {
 
     fetchCurrencyData(currency, base_currency);
 }
-
-// fetchData();
